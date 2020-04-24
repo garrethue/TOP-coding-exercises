ae019a6d3fbca33425d104449b8a2df4e6e40073
Garret Larson <garret.hs.larson@gmail.com>
2020-04-24 16:24:39 -0500

    Completed findTheOldest. All tests passed.

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
+let findTheOldest = function(arrayPersons) {
 
+  const arrayYearsOld = arrayPersons.map(objPerson => {
+    if (isNaN(objPerson.yearOfDeath - objPerson.yearOfBirth)){
+      let objDate = new Date();
+      return( {name: objPerson.name,
+        age: objDate.getFullYear() - objPerson.yearOfBirth})
+    }
+    else {
+      return ( {name: objPerson.name,
+                age: objPerson.yearOfDeath - objPerson.yearOfBirth} );
+    }
+  });
+
+  const arraySortedByAge = arrayYearsOld.sort((person1, person2) =>
+    person1.age > person2.age ? 1 : -1
+  );
+
+return arraySortedByAge[arraySortedByAge.length-1];
 }
 
 module.exports = findTheOldest
