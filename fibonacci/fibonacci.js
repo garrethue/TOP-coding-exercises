474abd441c6c8b1c5344672b0a30b8544606788c
Garret Larson <garret.hs.larson@gmail.com>
2020-04-24 06:05:57 -0500

    Completed fibonacci exercise. All tests passed.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(theXthMember) {
+  if (theXthMember < 0) {
+    return "OOPS"
+  }
+  let fibonacciSeries = [];
+  let oneAway, twoAway;
+  for (var i = 0; i < (theXthMember+1); i++) {
+    if (i === 0 || i === 1) { //edge case
+      fibonacciSeries.push(1);
+    }
+    else{ //look at previous two enties, add them, and push
+      oneAway = fibonacciSeries[i - 1];
+      twoAway = fibonacciSeries[i - 2];
+      fibonacciSeries.push(oneAway+twoAway);
+    }
+  }
+  return fibonacciSeries[theXthMember-1];
 }
 
 module.exports = fibonacci
