64ddf88f41e6762b723c5e6ccf57d1808545a29d
Garret Larson <garret.hs.larson@gmail.com>
2020-04-24 18:58:10 -0500

    Completed getTheTitles coding exercise. All tests passed.

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayBooks) {
+  const arrayTitles = arrayBooks.map(objBook => {
+    return objBook.title;
+  }
+  );
+  return arrayTitles
 }
 
 module.exports = getTheTitles;
